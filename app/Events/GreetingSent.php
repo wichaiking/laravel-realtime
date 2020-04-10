b664a4b91667db3e3a7cdaf6e5f51015b3787c74
Wichai Wimonkhachonyot <wiwichai@gmail.com>
2020-04-10 22:38:55 +0700

    update greeting event

--- app/Events/GreetingSent.php
+++ app/Events/GreetingSent.php
@@ -9,7 +9,6 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
 
 class GreetingSent implements ShouldBroadcast
 {
@@ -37,7 +36,6 @@ class GreetingSent implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        Log::debug($this->message);
         return new PrivateChannel("chat.greet.{$this->user->id}");
     }
 }
